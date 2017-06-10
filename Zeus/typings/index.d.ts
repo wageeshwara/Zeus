944ef6854a797a09b0bdddbdf504c15596a8179e
wageeshwara <wageeshwara@DESKTOP-B0SQ69O>
2017-06-11 00:29:34 +0530

    Add project files.

---
+++ Zeus/typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/bootstrap/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/mongoose/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
---
+++ Zeus/views/loginController.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', function ($scope,$log,$filter) {
+            console.log($log);
+            $log.log("This is log");
+            $log.info("This is information");
+            $log.warn("Wanrinig");
+            $log.debug("Debug inforamtion");
+            $log.error("This is error");
+            $scope.LoginResult = 'None';
+
+            $scope.login = function() {
+                $scope.LoginResult = 'Button works ' + $scope.username;
+
+            };
+
+            $scope.password = '';
+
+            $scope.lowerCasepass = function () {
+                return $filter('uppercase')($scope.password);
+            };
+        });
+})();
